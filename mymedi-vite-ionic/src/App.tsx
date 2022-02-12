c4dd2302ffb4e00786df51fcd7c61d7f4113b3e0
Constantin Ermicioi <costelerm@gmail.com>
2022-02-12 05:30:27 +0200

    Added some corrections

--- mymedi-vite-ionic/src/App.tsx
+++ mymedi-vite-ionic/src/App.tsx
@@ -1,8 +1,12 @@
-import { AddInformationButton } from "./components/add-information-button/AddInformationButton";
+import { setupIonicReact } from "@ionic/react";
+
 import { AddPersonPage } from "./components/add-person-page/AddPersonPage";
 
+import "@ionic/react/css/core.css";
+
+setupIonicReact();
+
 function App() {
-  // return <AddInformationButton></AddInformationButton>;
   return <AddPersonPage></AddPersonPage>;
 }
 
