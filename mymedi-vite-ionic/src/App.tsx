275ca0149726eed717d6e0982e159dd4e76e44d3
Constantin Ermicioi <costelerm@gmail.com>
2022-02-12 10:18:01 +0200

    Changed App file content

--- mymedi-vite-ionic/src/App.tsx
+++ mymedi-vite-ionic/src/App.tsx
@@ -1,13 +1,15 @@
-import { setupIonicReact } from "@ionic/react";
+import { IonApp, setupIonicReact } from "@ionic/react";
+import { AddInformationButton } from "./components/add-information-button/AddInformationButton";
 
 import { AddPersonPage } from "./components/add-person-page/AddPersonPage";
 
-import "@ionic/react/css/core.css";
-
-setupIonicReact();
-
 function App() {
-  return <AddPersonPage></AddPersonPage>;
+  return (
+    <IonApp>
+      <AddPersonPage></AddPersonPage>
+      <AddInformationButton></AddInformationButton>
+    </IonApp>
+  );
 }
 
 export default App;
