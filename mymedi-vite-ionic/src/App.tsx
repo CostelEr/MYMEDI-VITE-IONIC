a6aa2d6a694c1a8179f9fe78dc9af609ab7acd0f
Constantin Ermicioi <costelerm@gmail.com>
2022-02-16 21:06:27 +0200

    Installed Tailwind CSS

--- mymedi-vite-ionic/src/App.tsx
+++ mymedi-vite-ionic/src/App.tsx
@@ -2,11 +2,13 @@ import { IonApp } from "@ionic/react";
 
 import { AddInformationButton } from "./components/add-information-button/AddInformationButton";
 import { AddPersonPage } from "./components/add-person-page/AddPersonPage";
+import "./output.css";
 
 function App() {
   return (
     <IonApp>
       <AddPersonPage></AddPersonPage>
+      <div className="border-2 bg-gray-800 text-white">Salut</div>
       <AddInformationButton></AddInformationButton>
     </IonApp>
   );
