3ed5a00c6f570dc8c6ed47dbb91dd78fc60dd27c
Constantin Ermicioi <costelerm@gmail.com>
2022-02-19 10:13:48 +0200

    Created TimeLine component

--- mymedi-vite-ionic/src/components/time-line/TimeLine.tsx
+++ mymedi-vite-ionic/src/components/time-line/TimeLine.tsx
@@ -1,24 +1,136 @@
 import React from "react";
-import { IonList, IonItem, IonLabel } from "@ionic/react";
+import {
+  IonList,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonListHeader,
+  IonAvatar,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonApp,
+  IonText,
+} from "@ionic/react";
+import "../../index.css";
 
 export function TimeLine() {
   return (
-    <IonList>
-      <IonItem>
-        <IonLabel>Pokémon Yellow</IonLabel>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Mega Man X</IonLabel>
-      </IonItem>
-      <IonItem>
-        <IonLabel>The Legend of Zelda</IonLabel>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Pac-Man</IonLabel>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Super Mario World</IonLabel>
-      </IonItem>
-    </IonList>
+    <IonApp>
+      <IonHeader translucent>
+        <IonToolbar>
+          <IonTitle>TimeLine</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        <IonList>
+          <IonListHeader>
+            <IonText color="secondary">
+              <h3> LUNI, 14 FEBRUARIE, 2022</h3>{" "}
+            </IonText>
+          </IonListHeader>
+          <IonItem>
+            <IonLabel>
+              <div className="flex justify-start align-baseline">
+                <div className="text-2xl font-bold w-4/5">
+                  <IonText color="danger">
+                    <h2>
+                      38,0<sup>0</sup> Mild Fever
+                    </h2>
+                  </IonText>
+                </div>
+                <div className="w-1/5 flex justify-center">
+                  <IonText color="dark">
+                    <h3>9:48</h3>
+                  </IonText>
+                </div>
+              </div>
+              <p>Oral Reading - Manual Entry</p>
+            </IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonLabel>
+              <div className="flex justify-start align-baseline">
+                <div className="text-2xl font-bold w-4/5">
+                  <IonText color="dark">
+                    <h2>No Symptoms</h2>
+                  </IonText>
+                </div>
+                <div className="w-1/5 flex justify-center">
+                  <IonText color="dark">
+                    <h3>10:02</h3>
+                  </IonText>
+                </div>
+              </div>
+            </IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonLabel>
+              <div className="flex justify-start align-baseline">
+                <div className="text-2xl font-bold w-4/5">
+                  <IonText color="primary">
+                    <h2>
+                      36,5<sup>0</sup> No Fever
+                    </h2>
+                  </IonText>
+                </div>
+                <div className="w-1/5 flex justify-center">
+                  <IonText color="dark">
+                    <h3>9:48</h3>
+                  </IonText>
+                </div>
+              </div>
+
+              <p>Oral Reading - Manual Entry</p>
+            </IonLabel>
+          </IonItem>
+        </IonList>
+        <IonListHeader>
+          <IonText color="secondary">
+            <h3>MARȚI, 15 FEBRUARIE, 2022</h3>
+          </IonText>
+        </IonListHeader>
+        <IonItem>
+          <IonLabel>
+            <div className="flex justify-start align-baseline">
+              <div className="text-2xl font-bold w-4/5">
+                <IonText color="dark">
+                  <h2>No Symptoms</h2>
+                </IonText>
+              </div>
+              <div className="w-1/5 flex justify-center">
+                <IonText color="dark">
+                  <h3>10:02</h3>
+                </IonText>
+              </div>
+            </div>
+          </IonLabel>
+        </IonItem>
+
+        <IonItem>
+          <IonLabel>
+            <div className="flex justify-start align-baseline">
+              <div className="text-2xl font-bold w-4/5">
+                <IonText color="primary">
+                  <h2>
+                    36,5<sup>0</sup> No Fever
+                  </h2>
+                </IonText>
+              </div>
+              <div className="w-1/5 flex justify-center">
+                <IonText color="dark">
+                  <h3>9:48</h3>
+                </IonText>
+              </div>
+            </div>
+
+            <p>Oral Reading - Manual Entry</p>
+          </IonLabel>
+        </IonItem>
+      </IonContent>
+    </IonApp>
   );
 }
