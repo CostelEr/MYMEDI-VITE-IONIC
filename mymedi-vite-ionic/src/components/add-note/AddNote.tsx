8cca1dfcbb4943300419d63fe6849a6e2f95e56a
Constantin Ermicioi <costelerm@gmail.com>
2022-03-08 21:46:59 +0200

    Modified AddNote component

--- mymedi-vite-ionic/src/components/add-note/AddNote.tsx
+++ mymedi-vite-ionic/src/components/add-note/AddNote.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   IonItem,
   IonLabel,
@@ -6,21 +7,33 @@ import {
   IonButton,
 } from "@ionic/react";
 export function AddNote() {
+  const [note, setNote] = useState("");
+  const [notiteNoi, setNotiteNoi] = useState("");
   return (
     <div>
       <IonItem>
         <IonLabel position="stacked">Date and Time</IonLabel>
         <IonInput placeholder="24 februarie 2022" type="text"></IonInput>
       </IonItem>
-
       <IonItem>
         <IonLabel position="stacked">Titlu notiței</IonLabel>
         <IonInput placeholder="Titlu notiței" type="text"></IonInput>
       </IonItem>
       <IonItem>
         <IonLabel position="stacked">Conținutul notiței</IonLabel>
-        <IonTextarea placeholder="Conținutul notiței"></IonTextarea>
+        <IonTextarea
+          placeholder="Conținutul notiței"
+          onIonChange={(e) => {
+            setNote(e.detail.value!);
+            setNotiteNoi("true");
+          }}
+        ></IonTextarea>
       </IonItem>
+      {notiteNoi && (
+        <div className="bg-red-700 text-2xl text-white flex justify-center py-2">
+          {note}
+        </div>
+      )}
     </div>
   );
 }
