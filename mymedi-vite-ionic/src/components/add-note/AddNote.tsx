6aa18185ecd49676780483979add13a8bd2dd12e
Constantin Ermicioi <costelerm@gmail.com>
2022-04-30 07:56:33 +0300

    Added datetime in title for AddNote component

--- mymedi-vite-ionic/src/components/add-note/AddNote.tsx
+++ mymedi-vite-ionic/src/components/add-note/AddNote.tsx
@@ -1,32 +1,163 @@
-import { useState, ChangeEvent, FormEventHandler } from "react";
+import { useState } from "react";
 import { IonItem, IonLabel, IonInput, IonButton } from "@ionic/react";
-import { DateTimeReact } from "../date-time-react/DateTimeReact";
-import { DatetimeOptions } from "@ionic/core";
 
 interface INote {
   noteName: string;
   noteContent: string;
 }
 
-export function AddNote({ onCloseNote }) {
+export function AddNote() {
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const [noteList, setNoteList] = useState<INote[]>([]);
-  // const [today, setToday] = useState(DateTimeReact);
+
+  const weekday = [
+    "duminică",
+    "luni",
+    "marți",
+    "miercuri",
+    "joi",
+    "vineri",
+    "sâmbătă",
+  ];
+  const yearmounth = [
+    "ianuarie",
+    "februarie",
+    "martie",
+    "aprilie",
+    "mai",
+    "iunie",
+    "iulie",
+    "august",
+    "septembrie",
+    "octombrie",
+    "noiembrie",
+    "decembrie",
+  ];
+
+  const dayhours = [
+    "00",
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "00",
+  ];
+
+  const hoursminutes = [
+    "00",
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+    "29",
+    "30",
+    "31",
+    "32",
+    "33",
+    "34",
+    "35",
+    "36",
+    "37",
+    "38",
+    "39",
+    "40",
+    "41",
+    "42",
+    "43",
+    "44",
+    "45",
+    "46",
+    "47",
+    "48",
+    "49",
+    "50",
+    "51",
+    "52",
+    "53",
+    "54",
+    "55",
+    "56",
+    "57",
+    "58",
+    "59",
+    "00",
+  ];
+
+  const d = new Date();
+  let day = weekday[d.getDay()];
+  let mounth = yearmounth[d.getMonth()];
+  let hours = dayhours[d.getHours()];
+  let minutes = hoursminutes[d.getMinutes()];
 
   const addNote = (): void => {
     const newNote = { noteName: title, noteContent: content };
     setNoteList([...noteList, newNote]);
     setTitle(" ");
     setContent(" ");
-    onCloseNote(false);
   };
 
   return (
     <div>
       <IonItem>
+        <IonLabel color="primary">
+          <div className="flex w-full">
+            <div className="w-1/2">
+              {day}, {d.getDate()} {mounth}
+            </div>
+            <div className="w-1/2">
+              {hours}:{minutes}
+            </div>
+          </div>
+        </IonLabel>
+      </IonItem>
+      <IonItem>
         <IonLabel position="stacked">Titlu notiței</IonLabel>
         <IonInput
+          color="primary"
           placeholder="Titlu notiței"
           type="text"
           value={title}
@@ -36,6 +167,7 @@ export function AddNote({ onCloseNote }) {
       <IonItem>
         <IonLabel position="stacked">Conținutul notiței</IonLabel>
         <IonInput
+          color="primary"
           placeholder="Conținutul notiței"
           value={content}
           onIonChange={(e) => setContent(e.detail.value!)}
@@ -51,16 +183,11 @@ export function AddNote({ onCloseNote }) {
           return (
             <div>
               <div>
-                <span className="bg-gray-800 text-white">
-                  {new Date().getFullYear()}||
-                  {new Date().getDate()}||
-                  {new Date().getMonth()}||
-                  {new Date().getHours()}:{new Date().getMinutes()}:
-                  {new Date().getSeconds()}
-                </span>
-                <span>{note.noteName}</span> <span>|</span>
-                <span className="text-yellow-400">{note.noteContent}</span>
-                <button>x</button>
+                <p>
+                  <span>{note.noteName}</span> <span>|</span>
+                  <span className="text-yellow-400">{note.noteContent}</span>
+                  <button className="text-white">x</button>
+                </p>
               </div>
               <br></br>
             </div>
