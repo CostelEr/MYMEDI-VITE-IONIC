e3d4a2f7df96bcf48e4ae27556c4bee63582a77b
Constantin Ermicioi <costelerm@gmail.com>
2022-05-07 09:03:46 +0300

    Added AddNoteDate

--- mymedi-vite-ionic/src/components/add-note/AddNote.tsx
+++ mymedi-vite-ionic/src/components/add-note/AddNote.tsx
@@ -7,136 +7,18 @@ import {
   IonTextarea,
 } from "@ionic/react";
 
-interface INote {
-  noteName: string;
-  noteContent: string;
-  noteDay: string;
-  noteMounth: string;
-  noteDate: number;
-  noteHours: string;
-  noteMinutes: string;
-}
+import {
+  INote,
+  weekday,
+  yearmounth,
+  dayhours,
+  hoursminutes,
+} from "./AddNoteDate";
 
 export function AddNote() {
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const [noteList, setNoteList] = useState<INote[]>([]);
-
-  const weekday = [
-    "duminică",
-    "luni",
-    "marți",
-    "miercuri",
-    "joi",
-    "vineri",
-    "sâmbătă",
-  ];
-  const yearmounth = [
-    "ianuarie",
-    "februarie",
-    "martie",
-    "aprilie",
-    "mai",
-    "iunie",
-    "iulie",
-    "august",
-    "septembrie",
-    "octombrie",
-    "noiembrie",
-    "decembrie",
-  ];
-
-  const dayhours = [
-    "00",
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23",
-    "00",
-  ];
-
-  const hoursminutes = [
-    "00",
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23",
-    "24",
-    "25",
-    "26",
-    "27",
-    "28",
-    "29",
-    "30",
-    "31",
-    "32",
-    "33",
-    "34",
-    "35",
-    "36",
-    "37",
-    "38",
-    "39",
-    "40",
-    "41",
-    "42",
-    "43",
-    "44",
-    "45",
-    "46",
-    "47",
-    "48",
-    "49",
-    "50",
-    "51",
-    "52",
-    "53",
-    "54",
-    "55",
-    "56",
-    "57",
-    "58",
-    "59",
-    "00",
-  ];
 
   const d = new Date();
   let day = weekday[d.getDay()];
